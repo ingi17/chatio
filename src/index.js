bc05f9dab8f4682bf0cf1886cadbbca6d649fda2
thorlaugarson <gudmundurht@ru.is>
2019-03-22 16:08:06 +0000

    fixed add error

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import chat from './reducers'
+import { addUser } from './actions'
+
 
 const store = createStore(chat)
+
+store.dispatch(addUser('Me'))
+store.dispatch(addUser('Condom'))
+
 
 ReactDOM.render(
   <Provider store={store}>
